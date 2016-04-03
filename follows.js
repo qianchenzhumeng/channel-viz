ce77fe5d07c2fe2cb12e9e775b3f5dfffb76d921
qianchenzhumeng <qianchenzhumeng@live.cn>
2016-04-04 00:00:29 +0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'DsI985IuUydzZ0kGkP3eMgPVjHpOsKlGJuaDQoz1Q9LQl4Wu', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [529547944], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '2days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
